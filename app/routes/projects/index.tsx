292ed647b3bf1efec4f231741e08e8d7d3e50c7d
Maz <mazoolander@me.com>
2025-10-26 15:51:25 +0100

    feat: add project sorting by date, switch font to Outfit, update yellow palette, and restyle filter buttons

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-
 import ProjectCard from "~/components/ProjectCard";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 import Pagination from "~/components/Pagination";
+import { AnimatePresence, motion } from "framer-motion";
 
-{
-  /* Loader */
-}
 export async function loader({
   request,
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
@@ -18,28 +15,67 @@ export async function loader({
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData as { projects: Project[] };
-  // console.log(projects);
-
+  const [selectedCategory, setSelectedCategory] = useState("All");
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
   const projectsPerPage = 10;
-  // Calculate total pages
-  const totalPages = Math.ceil(projects.length / projectsPerPage);
+  // Loader
+  const { projects } = loaderData as { projects: Project[] };
+  // Sort by date (newest first)
+  projects.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+  // Get unique Categories
+  const categories = [
+    "All",
+    ...new Set(projects.map((project) => project.category)),
+  ];
 
+  // Filter projects based on the category
+  const filteredProjects =
+    selectedCategory === "All"
+      ? projects
+      : projects.filter((project) => project.category === selectedCategory);
+
+  // Calculate total pages
+  const totalPages = Math.ceil(filteredProjects.length / projectsPerPage);
   // Get current pages projects
   const indexOfLast = currentPage * projectsPerPage;
   const indexOfFirst = indexOfLast - projectsPerPage;
-  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+  const currentProjects = filteredProjects.slice(indexOfFirst, indexOfLast);
 
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">Projects</h2>
-      <div className="grid gap-6 sm:grid-cols-2">
-        {currentProjects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
+      {/* Filter Categories */}
+      <div className="felx flex-wrap gap-2 mb-8">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => {
+              setSelectedCategory(category);
+              // reset pagination
+              setCurrentPage(1);
+            }}
+            className={`mr-3 mb-3 ${
+              selectedCategory === category
+                ? "btn-filter-active"
+                : "btn-filter-default"
+            }`}
+          >
+            {category}
+          </button>
         ))}
       </div>
+
+      <AnimatePresence mode="wait">
+        <motion.div layout className="grid gap-6 sm:grid-cols-2">
+          {currentProjects.map((project) => (
+            <motion.div key={project.id} layout>
+              <ProjectCard project={project} />
+            </motion.div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
+
       <Pagination
         totalPages={totalPages}
         currentPage={currentPage}
